fd4bfd0b9fcaf48073d0940f4d73811dab1aa31d
Narendra <narendra.pro1212@gmail.com>
2026-06-14 13:09:39 +0530

    style: format asset_paths

---
+++ lib/core/constants/asset_paths.dart
@@ -0,0 +1,21 @@
+class AssetPaths {
+  // Lottie
+  static const sunny = 'assets/lottie/sunny.json';
+  static const clearNight = 'assets/lottie/clear_night.json';
+  static const partlyCloudyDay = 'assets/lottie/partly_cloud.json';
+  static const partlyCloudyNight = 'assets/lottie/partly_cloud.json';
+  static const cloudy = 'assets/lottie/overcast.json';
+  static const overcast = 'assets/lottie/overcast.json';
+  static const foggy = 'assets/lottie/fog.json';
+  static const drizzle = 'assets/lottie/drizzle.json';
+  static const rainy = 'assets/lottie/heavy_rain.json';
+  static const heavyRain = 'assets/lottie/heavy_rain.json';
+  static const thunderstorm = 'assets/lottie/thunderstorm.json';
+  static const thunderRain = 'assets/lottie/thunderstorm.json';
+  static const snowy = 'assets/lottie/snow.json';
+  static const heavySnow = 'assets/lottie/snow.json';
+  static const blizzard = 'assets/lottie/blizzard.json';
+  static const icePellets = 'assets/lottie/snow.json';
+  static const splashLogo = 'assets/lottie/sunny.json';
+  static const cloudRefresh = 'assets/lottie/pull_to_refresh_cloud.json';
+}
